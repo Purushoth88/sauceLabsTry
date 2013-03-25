93773727029e0c4d54c7b70a4d1725f4ea1d9b77
Ciprian I. Ileana <ciprian.ileana@trimplement.com>
2013-03-26 00:03:15 +0200

    Extracted constants. Added missing JavaDoc.

--- sauceLabsClient/src/test/java/com/test/poc/sauceLabs/util/flows/FlowsUtil.java
+++ sauceLabsClient/src/test/java/com/test/poc/sauceLabs/util/flows/FlowsUtil.java
@@ -10,16 +10,32 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 /**
+ * Utility class that provides Selenium flows used across tests
+ * 
  * @author Ciprian I. Ileana
  * @author Nicolae Petridean
- * 
  */
 public class FlowsUtil {
 
 	/**
 	 * class logger.
 	 */
-	private static final Logger	logger	= Logger.getLogger(FlowsUtil.class);
+	private static final Logger	logger				= Logger.getLogger(FlowsUtil.class);
+
+	/**
+	 * The valid URL for CaB Checkout
+	 */
+	private static final String	CAB_CHECKOUT_URL	= "https://api.clickandbuy-d2.com/webservices/automation/pay_1_1_0/LDVYSTJgeKhoEXloJ6b*U5ityFmIKFEWyE6-D8n7sdCjJ9wde5tXFjkjgeEVP31AdCxQCWecF80oPvXW*snXtrE5eMYiw2*3yYTCmCth5a*lPdmrOtnSKgIRJMAGBxTVi2stCBG2bf75d6HcXZA0X8UQ8e4DbBSqKk2ZBIuGsH*nBnCO4flsj6nuObHZE4nIWhbvBpyvzy6aEcwcnWC*v*YMJhcxGh8-eZCamP-M2L7GfeAwbEkl3LBWN8B4f0qZYIathizm350ABfxF9t5aI-cbNLnzh10grL48JbnaMItmeagsAna9*Cq2HWndcMAF3ldAPYARJqSlMgOpRHcWIscKQ7TwvpBFmbQmIQ2TWBBNE3E3XctYoMIQFnsjc7FU";
+
+	/**
+	 * A valid CaB login email to be used on Checkout With Existing Account flow
+	 */
+	private static final String	CAB_LOGIN_EMAIL		= "thijs@consumer.com";
+
+	/**
+	 * A valid CaB login password to be used on Checkout With Existing Account flow
+	 */
+	private static final String	CAB_LOGIN_PASSWORD	= "123@test";
 
 	/**
 	 * Private C'tor. Prevents instantiation.
@@ -34,12 +50,11 @@ public class FlowsUtil {
 	 */
 	public static void cabCheckoutWithExistingAccount(final WebDriver webDriver) {
 		logger.info("Starting CaB checkout flow");
-		webDriver
-				.get("https://api.clickandbuy-d2.com/webservices/automation/pay_1_1_0/LDVYSTJgeKhoEXloJ6b*U5ityFmIKFEWyE6-D8n7sdCjJ9wde5tXFjkjgeEVP31AdCxQCWecF80oPvXW*snXtrE5eMYiw2*3yYTCmCth5a*lPdmrOtnSKgIRJMAGBxTVi2stCBG2bf75d6HcXZA0X8UQ8e4DbBSqKk2ZBIuGsH*nBnCO4flsj6nuObHZE4nIWhbvBpyvzy6aEcwcnWC*v*YMJhcxGh8-eZCamP-M2L7GfeAwbEkl3LBWN8B4f0qZYIathizm350ABfxF9t5aI-cbNLnzh10grL48JbnaMItmeagsAna9*Cq2HWndcMAF3ldAPYARJqSlMgOpRHcWIscKQ7TwvpBFmbQmIQ2TWBBNE3E3XctYoMIQFnsjc7FU");
+		webDriver.get(CAB_CHECKOUT_URL);
 		webDriver.findElement(By.id("login_email")).clear();
-		webDriver.findElement(By.id("login_email")).sendKeys("thijs@consumer.com");
+		webDriver.findElement(By.id("login_email")).sendKeys(CAB_LOGIN_EMAIL);
 		webDriver.findElement(By.id("login_password")).clear();
-		webDriver.findElement(By.id("login_password")).sendKeys("123@test");
+		webDriver.findElement(By.id("login_password")).sendKeys(CAB_LOGIN_PASSWORD);
 		webDriver.findElement(By.id("login_submit")).click();
 
 		logger.info("waiting for page to load, after subit");
